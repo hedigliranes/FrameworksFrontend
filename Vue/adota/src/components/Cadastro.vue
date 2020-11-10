541f0d4669c73efa6aa24f025686b1cbb260d679
Alison Hedigliranes <alison.hedigliranes@gmail.com>
2020-11-10 13:29:35 -0300

    formulario vue

--- Vue/adota/src/components/Cadastro.vue
+++ Vue/adota/src/components/Cadastro.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <b-modal @ok="cadastrar"
+        <b-modal :ok-disabled="erros > 0" @ok="cadastrar"
          ok-title = "Cadastrar" ref="my-modal" id="modal-1" title="Cadastrar o Doguinho">
             <b-form-group id="input-group-2" label="Nome:" label-for="input-2">
              <b-form-input
@@ -8,17 +8,24 @@
                 v-model="nome"
                 required
                 placeholder="Nome do Doguinho"
+                @blur="checkField('nome')"
                 >
                 </b-form-input>
+                <br/>
+            <div class="error" style="color:red">{{ nameError }}</div>
                 <br/>
             <label for="textarea">Descrição:</label>
             <b-form-textarea
                 id="textarea"
+                @blur="checkField('desc')"
                 v-model="desc"
                 placeholder="Fale Sobre Ele"
                 rows="6"
                 max-rows="6"
         ></b-form-textarea>
+            <br/>
+            <div class="error" style="color:red">{{ nameInfo }}</div>
+            <br/>
             </b-form-group>
             
         </b-modal>
@@ -28,6 +35,25 @@
 <script>
 import axios from 'axios';
 
+const validate = {
+  desc: (value) => minLengthValidation(3, value),
+  nome: requiredValidation
+}
+
+export function minLengthValidation(minLength, value) {
+  if (!value || value.trim().length < minLength) {
+    return `Este campo requer pelo menos ${minLength} caracteres`
+  }
+  return null
+}
+
+export function requiredValidation(value) {
+  if (!value || value.trim() === '') {
+    return 'Este campo é obrigatório'
+  }
+  return null
+}
+
 export default {
   name: 'Cadastro',
 
@@ -36,7 +62,10 @@ export default {
       current: 0,
       nome: "",
       desc: "",
-      img: ""
+      img: "",
+      erros: 2,
+      nameError: " ",
+      nameInfo: " "
     } 
   },
 
@@ -46,7 +75,47 @@ export default {
       },
       cadastrar: function() {
         this.$emit("cadastrar", this.nome, this.desc, this.img);
-      }
+      },
+      checkField(name) {
+        let count = this.erros;
+        const value = this._data[name]
+        const error = validate[name] ? validate[name](value) : null        
+        if(count > 0){
+
+              if(error != null){
+                if(name == "nome"){
+                  this.nameError = error;
+                }else if(name == "desc"){
+                  this.nameInfo = error;
+                }
+              }
+
+              if(error != null && this.erros < 2){
+                count++;
+              }else if(error == null){
+                if(name == "nome" && this.nameError != ""){
+                  count--;
+                  this.nameError = "";
+                }else if(name == "desc" && this.nameInfo != ""){
+                  count--;
+                  this.nameInfo = "";
+                }
+              }
+        }else if(count == 0){
+
+              if(error != null){
+                count++;
+                 if(name == "nome"){
+                  this.nameError = error;
+                }else if(name == "desc"){
+                  this.nameInfo = error;
+                }
+            }
+          }
+          console.log(error)
+          this.erros = count;
+          console.log(this.erros)
+    }
     },
   mounted(){
       this.showModal();
